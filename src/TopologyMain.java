7f76d2b9ab611723c8acad6a547c54a1229eca8b
rchiky <raja.chiky@isep.fr>
2015-09-10 14:44:03 +0200

    ajout

---
+++ src/TopologyMain.java
@@ -0,0 +1,17 @@
+import spouts.WordReader;
+import backtype.storm.Config;
+import backtype.storm.LocalCluster;
+import backtype.storm.topology.TopologyBuilder;
+import backtype.storm.tuple.Fields;
+import backtype.storm.utils.Utils;
+import bolts.WordCounter;
+import bolts.WordNormalizer;
+
+
+public class TopologyMain {
+	public static void main(String[] args) throws InterruptedException {
+     
+   
+	}
+}
+
